756be3d777cfd53e5550d6fe835fec9d3a137528
choon83 <patch.yee@gmail.com>
2022-03-28 16:03:00 +0200

    Update 2

---
+++ src/main/java/se/lexicon/choon/Array.java
@@ -0,0 +1,18 @@
+package se.lexicon.choon;
+
+public class Array {
+
+    //1. Write a program which will store elements in an array of type ‘int’ and
+    //print it out.
+    //Expected output: 11 23 39 etc.
+
+    private int[] numbers = {
+            "11, 23, 39, 45"
+    };
+
+    public static void main(String[] args) {
+
+        numbers = new numbers();
+        System.out.println(numbers);
+    }
+}
---
+++ src/main/java/se/lexicon/choon/Car.java
@@ -0,0 +1,21 @@
+package se.lexicon.choon;
+
+Car
+
+public class Car {
+
+    String carBrand
+    String carReg
+    int carSpeed
+
+    public static void main (String[]args) {
+
+        Car peugeot = new Car();
+        peugeot.carBrand = "Peugeot";
+        peugeot.carReg = "ABC12A";
+        peugeot.carSpeed = 200;
+
+
+
+    }
+}
