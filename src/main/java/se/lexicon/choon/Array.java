57eaf4b8706419fcb06563a8b2afbc36b6199cad
choon83 <patch.yee@gmail.com>
2022-03-29 11:32:16 +0200

    Uploading Array.java
    Still in progress.

--- src/main/java/se/lexicon/choon/Array.java
+++ src/main/java/se/lexicon/choon/Array.java
@@ -6,26 +6,25 @@ public class Array {
     //print it out.
     //Expected output: 11 23 39 etc.
 
-    numbers();
 
-
-    int[] numbers = new int[4];
-
-    numbers[0] = 11;
-    numbers[1] = 23;
-    numbers[2] = 39;
-    numbers[3] = 45;
-/*
-    private int[] numbers = {
-            "11, 23, 39, 45"
-    };
-    */
 
 
     public static void main(String[] args) {
 
+        int[] numbers = new int[5];
 
-        for (int value : numbers) {
+        numbers[0] = 11;
+        numbers[1] = 23;
+        numbers[2] = 39;
+        numbers[3] = 45;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int value = numbers[i];
             System.out.println(value);
+
+    }
+
+
+
     }
 }
