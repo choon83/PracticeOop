914e6e7e2ea90a6395e16bb4b785b85489da78c1
choon83 <patch.yee@gmail.com>
2022-03-30 13:00:02 +0200

    - Strings update #1 Cant get the code to run...

---
+++ src/main/java/se/lexicon/choon/Strings.java
@@ -0,0 +1,29 @@
+package se.lexicon.choon;
+
+public class Strings {
+
+// Can't get it to run....
+
+
+/*
+    public static void main(String[] args) {
+
+
+        //What is the length of the String: "Java" ?
+        String name = "Java";
+        System.out.println("The word contains: "+name.length()) + " letters.";
+
+*/
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
